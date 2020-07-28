d8623a141ed5b376b56fdd8c67cb72c13c69b362
Ben Schwartz <ben.schwartz@cybergrx.com>
2020-07-27 19:53:07 -0600

    Added permissions around queries and mutations

---
+++ src/permissions/index.ts
@@ -0,0 +1,21 @@
+import { rule, shield } from "graphql-shield"
+
+const isAuthenticated = rule()((parent, args, { userId }) => {
+  return !!userId
+})
+
+export const permissions = shield({
+  Query: {
+    // OrderSet Queries
+    orderSet: isAuthenticated,
+    groupOrderSets: isAuthenticated,
+  },
+  Mutation: {
+    // OrderSet Mutations
+    createOrderSet: isAuthenticated,
+    updateOrderSet: isAuthenticated,
+
+    // Order Mutations
+    cancelOrder: isAuthenticated,
+  },
+})
