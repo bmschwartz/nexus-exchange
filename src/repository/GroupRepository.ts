74cf2b8fb09bf4afd91ad5f5a1fe5ff4d106961a
Ben Schwartz <schwartz.ben0@gmail.com>
2020-10-02 18:31:57 -0600

    Setup cursor-based pagination for group ordersets

--- src/repository/GroupRepository.ts
+++ src/repository/GroupRepository.ts
@@ -1,6 +1,42 @@
-import { OrderSet } from "@prisma/client";
+import { FindManyOrderSetArgs, OrderSet } from "@prisma/client";
 import { Context } from "src/context";
 
-export const getGroupOrderSets = async (ctx: Context, groupId: number): Promise<OrderSet[]> => {
-    return ctx.prisma.orderSet.findMany({ where: { groupId } })
+export interface IOrderSetsResult {
+  orderSets: OrderSet[]
+  hasMore: Boolean
+}
+
+export interface IOrderSetsInput {
+  groupId: number
+  pageSize?: number
+  after?: string
+}
+
+const DEFAULT_PAGE_SIZE = 25
+
+export const getGroupOrderSets = async (ctx: Context, { groupId, pageSize, after }: IOrderSetsInput): Promise<IOrderSetsResult> => {
+  pageSize = pageSize || DEFAULT_PAGE_SIZE
+
+  const findArgs: FindManyOrderSetArgs = {
+    take: pageSize,
+    where: { groupId },
+    orderBy: { id: "desc" }
+  }
+
+  if (after) {
+    findArgs.skip = 1
+    findArgs.cursor = { id: Number(after) }
+  }
+
+  const orderSets = await ctx.prisma.orderSet.findMany(findArgs)
+
+  const lastOrderSet = await ctx.prisma.orderSet.findFirst({
+    where: { groupId },
+    orderBy: { id: "asc" }
+  })
+
+  return {
+    orderSets,
+    hasMore: lastOrderSet.id !== orderSets[orderSets.length - 1].id,
+  }
 }
--- src/schema/resolvers/GroupResolvers.ts
+++ src/schema/resolvers/GroupResolvers.ts
@@ -2,7 +2,13 @@ import { Context } from "../../context"
 import { getGroupOrderSets } from "../../repository/GroupRepository"
 
 export const GroupResolvers = {
-  async orderSets(group: any, args: any, ctx: Context) {
-    return getGroupOrderSets(ctx, Number(group.id))
+  async orderSets(group: any, { pageSize, after }: any, ctx: Context) {
+    const { orderSets, hasMore } = await getGroupOrderSets(ctx, { after, pageSize, groupId: Number(group.id) })
+
+    return {
+      hasMore,
+      orderSets,
+      cursor: orderSets.length ? orderSets[orderSets.length - 1].id : null,
+    }
   },
 }
