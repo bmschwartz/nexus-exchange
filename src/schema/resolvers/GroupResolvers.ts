d5048dec5a4ab47433c4ce7cb4718dd2a4bca8c2
Ben Schwartz <schwartz.ben0@gmail.com>
2020-10-02 23:17:17 -0600

    Adds an orderSet detail query to the Group type

--- src/schema/resolvers/GroupResolvers.ts
+++ src/schema/resolvers/GroupResolvers.ts
@@ -1,7 +1,12 @@
 import { Context } from "../../context"
+import { getOrderSet } from "../../repository/OrderSetRepository"
 import { getGroupOrderSets } from "../../repository/GroupRepository"
 
 export const GroupResolvers = {
+  async orderSet(group: any, args: any, ctx: Context) {
+    return getOrderSet(ctx, Number(args.input.id))
+  },
+
   async orderSets(group: any, { limit, offset }: any, ctx: Context) {
     return getGroupOrderSets(ctx, { limit, offset, groupId: Number(group.id) })
   },
