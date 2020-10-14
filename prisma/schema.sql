0ce96addf4bf8ad4c46b39508e6caba6d624bf02
Ben Schwartz <schwartz.ben0@gmail.com>
2020-10-14 15:58:30 -0600

    Remove SQS

--- prisma/schema.sql
+++ prisma/schema.sql
@@ -1,27 +1,33 @@
 DROP DATABASE IF EXISTS "nexus_exchange";
 CREATE DATABASE "nexus_exchange";
 
-CREATE TYPE ORDER_SIDE AS ENUM('BUY', 'SELL');
-CREATE TYPE ORDER_TYPE AS ENUM('MARKET', 'LIMIT');
-CREATE TYPE POSITION_SIDE AS ENUM('LONG', 'SHORT');
-CREATE TYPE EXCHANGE AS ENUM('BINANCE', 'BITMEX');
-CREATE TYPE BINANCE_SYMBOL_STATUS AS ENUM(
+CREATE TYPE "OrderSide" AS ENUM('BUY', 'SELL');
+CREATE TYPE "OrderType" AS ENUM('MARKET', 'LIMIT');
+CREATE TYPE "PositionSide" AS ENUM('LONG', 'SHORT');
+CREATE TYPE "Exchange" AS ENUM('BINANCE', 'BITMEX');
+CREATE TYPE "BinanceSymbolStatus" AS ENUM(
   'PRE_TRADING', 'TRADING', 'POST_TRADING',
   'END_OF_DAY', 'HALT', 'AUCTION_MATCH', 'BREAK'
 );
-CREATE TYPE ORDER_STATUS AS ENUM(
+CREATE TYPE "OrderStatus" AS ENUM(
   'NEW', 'FILLED', 'PARTIALLY_FILLED', 'CANCELED'
+);
+CREATE TYPE "OperationType" AS ENUM(
+  'CREATE_BINANCE_ORDER', 'UPDATE_BINANCE_ORDER', 'CANCEL_BINANCE_ORDER',
+  'CREATE_BINANCE_ACCOUNT', 'UPDATE_BINANCE_ACCOUNT', 'DELETE_BINANCE_ACCOUNT'
 );
 
 CREATE TABLE "public"."OrderSet" (
   id BIGSERIAL PRIMARY KEY NOT NULL,
+  exchange "Exchange" NOT NULL,
+  symbol VARCHAR(255) NOT NULL,
   "description" TEXT,
   "groupId" INTEGER NOT NULL,
-  "orderType" ORDER_TYPE NOT NULL,
+  "orderType" "OrderType" NOT NULL,
   "price" DECIMAL,
   "stopPrice" DECIMAL,
   "percent" DECIMAL NOT NULL,
-  "side" ORDER_SIDE NOT NULL,
+  "side" "OrderSide" NOT NULL,
   "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
   "updatedAt" TIMESTAMP NOT NULL DEFAULT now()
 );
@@ -31,15 +37,15 @@ CREATE TABLE "public"."Order" (
   "orderSetId" BIGINT NOT NULL,
   "exchangeAccountId" INTEGER NOT NULL,
   "symbol" VARCHAR(255) NOT NULL,
-  "exchange" EXCHANGE NOT NULL,
-  "side" ORDER_SIDE NOT NULL,
+  "exchange" "Exchange" NOT NULL,
+  "side" "OrderSide" NOT NULL,
   "lastTimestamp" TIMESTAMP,
-  "orderType" ORDER_TYPE,
+  "orderType" "OrderType",
   "price" DECIMAL,
   "stopPrice" DECIMAL,
   "quantity" DECIMAL,
   "filledQty" DECIMAL,
-  "status" ORDER_STATUS NOT NULL,
+  "status" "OrderStatus" NOT NULL,
   "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
   "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
   FOREIGN KEY ("orderSetId") REFERENCES "public"."OrderSet"(id),
@@ -50,8 +56,8 @@ CREATE TABLE "public"."Position" (
   id BIGSERIAL PRIMARY KEY NOT NULL,
   "exchangeAccountId" INTEGER NOT NULL,
   "symbol" VARCHAR(255) NOT NULL,
-  "exchange" EXCHANGE NOT NULL,
-  "side" POSITION_SIDE NOT NULL,
+  "exchange" "Exchange" NOT NULL,
+  "side" "PositionSide" NOT NULL,
   "avgPrice" DECIMAL,
   "quantity" DECIMAL,
   "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
@@ -75,7 +81,7 @@ CREATE TABLE "public"."BitmexCurrency" (
 CREATE TABLE "public"."BinanceCurrency" (
   id SERIAL PRIMARY KEY NOT NULL,
   symbol VARCHAR(255) UNIQUE NOT NULL,
-  "status" BINANCE_SYMBOL_STATUS NOT NULL,
+  "status" "BinanceSymbolStatus" NOT NULL,
   "baseAsset" VARCHAR(255) NOT NULL,
   "baseAssetPrecision" INTEGER,
   "quoteAsset" VARCHAR(255) NOT NULL,
@@ -129,8 +135,9 @@ CREATE TABLE "public"."BinanceCurrency" (
 CREATE TABLE "public"."ExchangeAccount" (
   id BIGSERIAL PRIMARY KEY NOT NULL,
   active BOOLEAN NOT NULL DEFAULT false,
-  exchange EXCHANGE NOT NULL,
+  exchange "Exchange" NOT NULL,
   "membershipId" INTEGER NOT NULL,
+  "remoteAccountId" VARCHAR(255),
   "apiKey" VARCHAR(255) NOT NULL,
   "apiSecret" VARCHAR(255) NOT NULL,
   "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
@@ -141,9 +148,11 @@ CREATE TABLE "public"."ExchangeAccount" (
 
 CREATE TABLE "public"."AsyncOperation" (
   id BIGSERIAL PRIMARY KEY NOT NULL,
+  "opType" "OperationType" NOT NULL,
   complete BOOLEAN NOT NULL DEFAULT false,
-  success BOOLEAN,
+  success BOOLEAN DEFAULT false,
   error VARCHAR(255),
+  payload JSONB,
   "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
   "updatedAt" TIMESTAMP NOT NULL DEFAULT now()
 );
@@ -160,13 +169,3 @@ $$;
 CREATE TRIGGER trigger_delete_old_async_operations
     AFTER INSERT ON "public"."AsyncOperation"
     EXECUTE PROCEDURE delete_old_async_operations();
-
-ALTER TABLE "public"."OrderSet"
-ADD COLUMN exchange EXCHANGE NOT NULL,
-ADD COLUMN symbol VARCHAR(255) NOT NULL;
-
-ALTER TABLE "public"."ExchangeAccount"
-  ADD COLUMN "remoteAccountId" VARCHAR(255);
-
-ALTER TABLE "public"."AsyncOperation"
-  ADD COLUMN payload JSONB;
