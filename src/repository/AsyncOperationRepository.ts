4ab49ba301ef77ca77d2c1616fe19c2d9fbf8787
Ben Schwartz <schwartz.ben0@gmail.com>
2020-10-17 09:27:10 -0600

    Returning operation if the user who created it matches the querying user for getAsyncOperation

--- src/repository/AsyncOperationRepository.ts
+++ src/repository/AsyncOperationRepository.ts
@@ -2,7 +2,8 @@ import { AsyncOperation } from "@prisma/client"
 import { Context } from "src/context"
 
 export const getAsyncOperation = async (ctx: Context, id: number): Promise<AsyncOperation | null> => {
-  return ctx.prisma.asyncOperation.findOne({
+  const op = await ctx.prisma.asyncOperation.findOne({
     where: { id }
   })
+  return op?.userId === ctx.userId ? op : null
 }
