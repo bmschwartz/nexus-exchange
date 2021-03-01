92a55d961273570089cd2320d103d8746541cc98
Ben Schwartz <schwartz.ben0@gmail.com>
2021-02-28 20:50:48 -0700

    Increase heartbeat timeout to 2 minutes

--- src/services/accountMonitor.ts
+++ src/services/accountMonitor.ts
@@ -10,9 +10,9 @@ let _db: PrismaClient
 let _messenger: MessageClient
 
 const CHECK_ACCOUNT_JOB = "checkAccountJob"
-const CHECK_ACCOUNT_INTERVAL = 10000 // ms
+const CHECK_ACCOUNT_INTERVAL = 10 * 1000 // ms
 
-const HEARTBEAT_TIMEOUT = 60000 // ms
+const HEARTBEAT_TIMEOUT = 2 * 60 * 1000 // ms
 
 async function _checkAccountLife(job: Job) {
   const timeoutDate = new Date(Date.now() - HEARTBEAT_TIMEOUT)
