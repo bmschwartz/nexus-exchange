0d29a5b89a7a023d79e0d5b531f43aa427bc47bb
Ben Schwartz <schwartz.ben0@gmail.com>
2021-03-07 15:01:19 -0700

    Tack on

--- src/context.ts
+++ src/context.ts
@@ -2,12 +2,14 @@ import { PrismaClient } from "@prisma/client"
 import { AccountMonitor } from "./services/accountMonitor"
 import { MessageClient } from "./services/messenger"
 import { initSettings } from "./settings"
+import {AsyncOpCleanupClient} from "./services/asyncOpCleanup";
 
 initSettings()
 
 export const prisma: PrismaClient = new PrismaClient()
 export const messenger = new MessageClient(prisma)
 export const accountMonitor = new AccountMonitor(prisma, messenger)
+export const asyncOpCleanUp = new AsyncOpCleanupClient(prisma)
 
 export interface Context {
   userId?: string
