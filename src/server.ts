f417bafba28639cc15d3616f4fe844d30c626872
Ben Schwartz <schwartz.ben0@gmail.com>
2021-03-17 22:05:14 -0600

    Use env var to specify port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from "apollo-server"
 import { buildFederatedSchema } from "@apollo/federation"
 import { applyMiddleware } from "graphql-middleware"
+import * as dotenv from "dotenv"
 
 import { typeDefs } from "./schema/types"
 import { resolvers } from "./schema/resolvers"
@@ -8,6 +9,9 @@ import { createContext } from "./context"
 import { permissions } from "./permissions"
 import { bootstrap } from "./bootstrap"
 
+dotenv.config()
+
+const port = process.env.PORT || 4001
 const graphVariant = process.env.APOLLO_GRAPH_VARIANT || "current"
 
 const server = new ApolloServer({
@@ -22,7 +26,7 @@ const server = new ApolloServer({
   introspection: true,
 })
 
-server.listen({ port: 4001 }).then(({ url }) => {
+server.listen({ port }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`)
   bootstrap()
 })
